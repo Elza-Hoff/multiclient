1eb68524880f1863393060225e776b828ded3705
Yelyzaveta <kartcevayelyzaveta@gmail.com>
2021-03-01 16:49:00 +0200

    Initial commit

---
+++ MultiClient/MultiClient/MultiClient.h
@@ -0,0 +1,18 @@
+//
+//  MultiClient.h
+//  MultiClient
+//
+//  Created by Yelyzaveta Kartseva on 01.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MultiClient.
+FOUNDATION_EXPORT double MultiClientVersionNumber;
+
+//! Project version string for MultiClient.
+FOUNDATION_EXPORT const unsigned char MultiClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultiClient/PublicHeader.h>
+
+
---
+++ MultiClient/MultiClient/Protocols/APIClientProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  APIClientProtocol.swift
+//  MultiClient
+//
+//  Created by Yelyzaveta Kartseva on 01.03.2021.
+//
+
+import Foundation
+
+protocol APIClientProtocol {
+    
+    var baseURL: String { get }
+    
+}
